a2ae1eb321cf32ffd533cd5751878d0568bacfb4
alice <bow1226@gmail.com>
2018-05-11 06:15:51 +0800

    Tune kp = 0.15, kd = 0.09 mn = decel_limit, mx = accel_limit
    Tune kp = 0.15, kd = 0.09 mn = decel_limit, mx = accel_limit

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -12,12 +12,12 @@ class Controller(object):
         
         self.yaw_controller = YawController(wheel_base, steer_ratio, 0.1, max_lat_accel, max_steer_angle)
 
-        kp = 5 #0.3
-        ki = 0.1#0.1
-        kd = 0.1#0.
+        kp = 0.15  # 0.3
+        ki = 0.0 # 0.1
+        kd = 0.09 # 0.
         mn = 0. # Minimum throttle value
         mx = 0.2 # Maxmium throttle value
-        self.throttle_controller = PID(kp, ki, kd, mn, mx)
+        self.throttle_controller = PID(kp, ki, kd, mn = decel_limit, mx = accel_limit)
 
         tau = 0.5 # 1/(2pi*tau) = cutoff frequency
         ts = .02 # Sample time
