128805ec37c966ee55706c9d79edb5227b012106
alice <bow1226@gmail.com>
2018-05-11 06:52:05 +0800

    Tune kp = 0.15, kd = 0.09 mn = decel_limit, mx = accel_limit
    Tune kp = 0.15, kd = 0.09 mn = decel_limit, mx = accel_limit & Format

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,26 +1,22 @@
+from yaw_controller import YawController
 from pid import PID
 from lowpass import LowPassFilter
-from yaw_controller import YawController
 import rospy
 
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
 
-
 class Controller(object):
-    #def __init__(self, *args, **kwargs):
-        # TODO: Implement
-    #    pass
     def __init__(self, vehicle_mass, fuel_capacity, brake_deadband, decel_limit, accel_limit, wheel_radius, wheel_base, steer_ratio, max_lat_accel, max_steer_angle):
         
         self.yaw_controller = YawController(wheel_base, steer_ratio, 0.1, max_lat_accel, max_steer_angle)
 
-        kp = 5 #0.3
-        ki = 0.1#0.1
-        kd = 0.1#0.
+        kp = 0.15 #0.3
+        ki = 0.0 # 0.1
+        kd = 0.09 # 0.
         mn = 0. # Minimum throttle value
         mx = 0.2 # Maxmium throttle value
-        self.throttle_controller = PID(kp, ki, kd, mn, mx)
+        self.throttle_controller = PID(kp, ki, kd, mn = decel_limit, mx = accel_limit)
 
         tau = 0.5 # 1/(2pi*tau) = cutoff frequency
         ts = .02 # Sample time
@@ -35,11 +31,6 @@ class Controller(object):
 
         self.last_time = rospy.get_time()
 
-
-    #def control(self, *args, **kwargs):
-        # TODO: Change the arg, kwarg list to suit your needs
-        # Return throttle, brake, steer
-        #return 1., 0., 0.
     def control(self, current_vel, dbw_enabled, linear_vel, angular_vel):
         if not dbw_enabled:
             self.throttle_controller.reset()
@@ -81,9 +72,4 @@ class Controller(object):
             brake = -max(accel + self.brake_deadband, self.decel_limit)*self.vehicle_mass*self.wheel_radius
 
             
-        return throttle, brake, steering 
-
-
-        
-
-
+        return throttle, brake, steering
